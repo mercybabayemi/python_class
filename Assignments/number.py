667c3fee33d29467685c59789dd48eca6d48b396
mercybabayemi <mercyjanet013@gmail.com>
2024-10-21 08:23:25 +0100

    add

---
+++ Assignments/number.py
@@ -0,0 +1,24 @@
+number = 0
+largestNumber = 0
+smallestNumber = 0
+	
+
+while True:
+    userInput = int(input("Enter number: "))
+		
+
+    if userInput > largestNumber:
+        largestNumber = userInput
+    if userInput < smallestNumber:
+        smallestNumber = userInput
+
+    answer = int(input("DO you wish to continue, press 1 for(Yes) or 2 for(no) to exit"))
+
+    if answer == 1:
+        continue
+    elif answer == 2:
+        break
+
+print(f"The largest number is {largestNumber}")
+print(f"The smallest number is {smallestNumber}")
+
