79474e12af5fc66ed3c185b9237f9973db146f7c
mercybabayemi <mercyjanet013@gmail.com>
2024-12-11 12:09:35 +0100

    update

---
+++ PhaseGateOne/Lagbaja school/lagbajaschoolgrading.py
@@ -0,0 +1,9 @@
+def getStudentNumber(teacher):
+	numberOfStudents = int(input(f"Mr/Mrs/Miss {teacher} how many students do you have? \n"))
+	print("Saving>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\nSaved successfully.\n")
+	return numberOfStudent
+
+def getSubjects(teacher):
+	studentSubjects = int(input(f"Mr/Mrs/Miss {teacher}, how many subjects do you want to grade the students on? \n"))
+	print("Saving>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\nSaved successfully.")
+	return studentSubjects
--- PhaseGateOne/dayoftheweekcalculated.py
+++ PhaseGateOne/dayoftheweekcalculated.py
@@ -1,62 +1,31 @@
 """
 pseudocode today's day of the week 
+- Declare days of the week
 - Prompt user to enter an integer for todays date 
 - collect and store response as option
 - prompt user to enter the numbers of days after today for a future date and display the future day of the week
 """
-	
 
-message = """ 
-	What is today's day?
-	Enter 0 for Sunday
-	Enter 1 for Monday
-	Enter 2 for Tuesday
-	Enter 3 for Wednesday
-	Enter 4 for Thursday
-	Enter 5 for Friday
-	Enter 6 for Saturday
-	"""
-todays_day =" "
+def get_calculation():
+	days = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 	
-option = int(input(message))
-	
-match option: 
-	case 0:
-		todays_date = "Sunday"
-		print(f"Today is {todays_date}.")
-	case 1:
-		todays_date = "Monday"
-		print(f"Today is {todays_date}.")
-	case 2: 
-		todays_date = "Tuesday"
-		print(f"Today is {todays_date}.")
-	case 3:
-		todays_date = "Wednesday"
-		print(f"Today is {todays_date}.")
-	case 4:
-		todays_date = "Thursday"
-		print(f"Today is {todays_date}.")
-	case 5:
-		todays_date = "Friday"
-		print(f"Today is {todays_date}.")
-	case 6:
-		todays_date = "Saturday"
-		print(f"Today is {todays_date}.")
-	case _:
-			print("Default, enter the right value!!")
-			get_day()
-
-def get_calculation(option,todays_date):
 	message = """ 
-		What is the future day?
-		Enter numerical value: 
-		"""
-	days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-	new_input = (input(message))
+		What is today's day?
+		Enter 0 for Sunday
+		Enter 1 for Monday
+		Enter 2 for Tuesday
+		Enter 3 for Wednesday
+		Enter 4 for Thursday
+		Enter 5 for Friday
+		Enter 6 for Saturday
+		"""	
+	option = int(input(message))
 
-	for index at range(new_input):
-		future_day += days[index]
+	new_input = int(input("What is the future day?\nHow many days from today?"))
+	future_day_index = (option + new_input) % 7
+	future_day = days[future_day_index]
 
+	print(f"The future day is {future_day}")
 		
 
-get_calculation(option,todays_date)
+get_calculation()
