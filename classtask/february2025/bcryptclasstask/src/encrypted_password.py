3dfc927c6548a55c56cf6b6beac248d8955791c7
mercybabayemi <mercyjanet013@gmail.com>
2025-02-20 12:20:01 +0100

    update bcrypt task

--- classtask/february2025/bcryptclasstask/src/encrypted_password.py
+++ classtask/february2025/bcryptclasstask/src/encrypted_password.py
@@ -5,9 +5,9 @@ USER_DETAILS = 'user_details.txt'
 def hash_password(password):
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
-def save_to_file(username, password):
+def save_to_file(username, hashed_password):
     with open(USER_DETAILS, 'a') as file:
-        file.write(f'{username},{hash_password(password).decode("utf-8")}\n')
+        file.write(f'{username}:{hashed_password.decode("utf-8")}\n')
 
 def register_user():
     while True:
@@ -16,6 +16,7 @@ def register_user():
             print("Username cannot be empty.")
             continue
         break
+
     while True:
         password = input("Enter your password: ")
         if not password:
@@ -23,27 +24,33 @@ def register_user():
             continue
         break
 
-    save_to_file(username,password)
+    save_to_file(username, hash_password(password))
     print('User registered successfully')
 
-def validate_user(username,password):
+def validate_user(username, password):
     with open(USER_DETAILS, 'r') as file:
-        details = file.read()
-        if not details:
-            print("No registered users found")
-            return False
-        for line in details.split('\n'):
-            if line.strip():
-                stored_username, stored_password = line.split(',')
-                if username == stored_username:
-                    if bcrypt.checkpw(password.encode('utf-8'), stored_password.encode('utf-8')):
-                        return True
-    return False
+        for line in file:
+            stored_username, stored_password = line.strip().split(':')
+            if username == stored_username:
+                return bcrypt.checkpw(password.encode('utf-8'), stored_password.encode('utf-8'))
+
+    # with open(USER_DETAILS, 'r') as file:
+    #     details = file.read()
+    #     if not details:
+    #         print("No registered users found")
+    #         return False
+    #     for line in details.split('\n'):
+    #         if line.strip():
+    #             stored_username, stored_password = line.split(',')
+    #             if username == stored_username:
+    #                 if bcrypt.checkpw(password.encode('utf-8'), stored_password.encode('utf-8')):
+    #                     return True
+    # return False
 
 def login_user():
     username = input("Enter your username: ")
     password = input("Enter your password: ")
-    if validate_user(username,password):
+    if validate_user(username, password):
         print("Login successful!")
     else:
         print("Invalid username or password")
