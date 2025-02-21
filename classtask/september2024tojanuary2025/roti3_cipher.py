a84154adbd8674a29b6234c3e762f028790bbff6
mercybabayemi <mercyjanet013@gmail.com>
2025-02-21 16:44:15 +0100

    update

--- classtask/september2024tojanuary2025/roti3_cipher.py
+++ classtask/september2024tojanuary2025/roti3_cipher.py
@@ -1,7 +1,6 @@
 def encrypt_text(text):
     encrypted = ""
-    for word in text:
-        for letter in word:
+    for letter in text:
             if letter.isalpha():
                 base = "a"
                 if letter.islower():
@@ -13,7 +12,7 @@ def encrypt_text(text):
                 encrypted += encrypted_char
             elif letter.isdigit():
                 base = "0"
-                encrypted_char_calculated = (ord(letter) - ord(base) + 13 + 9)% 9 + ord(base)
+                encrypted_char_calculated = (ord(letter) - ord(base) + 13 + 10)% 10 + ord(base)
                 encrypted_char = chr(encrypted_char_calculated)
                 encrypted += encrypted_char
             else:
