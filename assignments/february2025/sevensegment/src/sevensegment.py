875dbfc835b65f33f876c49ff5e0fb6d4764267e
mercybabayemi <mercyjanet013@gmail.com>
2025-02-28 13:33:23 +0100

    assignment

---
+++ assignments/february2025/sevensegment/src/sevensegment.py
@@ -0,0 +1,85 @@
+class SevenSegmentDisplay:
+    def __init__(self):
+        self._is_on = False
+        self.__board_capacity = 4
+        self.__board = [[' ' for i in range(self.__board_capacity)] for i in range(self.__board_capacity + 1)]
+
+    def get_is_on(self):
+        return self._is_on
+
+    def get_board(self):
+        return self.__board
+
+    def display_segment(self, digits):
+        self.validate(digits)
+        value = self.change_status(digits)
+        if value:
+            self.set_board(digits)
+        return self.get_board()
+
+    def change_status(self, digits):
+        if digits[7] == '1':
+            self._is_on = True
+            return self.get_is_on()
+        else:
+            self._is_on = False
+            return self.get_is_on()
+
+    def set_board(self, digits):
+
+        if digits[0] == '1':
+            self.__board[0][0] = '#'
+            self.__board[0][1] = '#'
+            self.__board[0][2] = '#'
+            self.__board[0][3] = '#'
+
+        if digits[1] == '1' and digits[2] == '1':
+            self.__board[1][3] = '#'
+            self.__board[2][3] = '#'
+            self.__board[3][3] = '#'
+        elif digits[1] == '1':
+            self.__board[1][3] = '#'
+            self.__board[2][3] = '#'
+        elif digits[2] == '1':
+            self.__board[2][3] = '#'
+            self.__board[3][3] = '#'
+
+        if digits[3] == '1':
+            self.__board[4][0] = '#'
+            self.__board[4][1] = '#'
+            self.__board[4][2] = '#'
+            self.__board[4][3] = '#'
+
+        if digits[4] == '1' and digits[5] == '1':
+            self.__board[1][0] = '#'
+            self.__board[2][0] = '#'
+            self.__board[3][0] = '#'
+        elif digits[1] == '1':
+            self.__board[1][0] = '#'
+            self.__board[2][0] = '#'
+        elif digits[2] == '1':
+            self.__board[2][0] = '#'
+            self.__board[3][0] = '#'
+
+        if digits[6] == '1':
+            self.__board[2][0] = '#'
+            self.__board[2][1] = '#'
+            self.__board[2][2] = '#'
+            self.__board[2][3] = '#'
+
+    def validate(self, digits):
+        if not digits.isdigit():
+            raise ValueError("Binary number must be only digits")
+        if not all(digit in '01' for digit in digits):
+            raise ValueError("Binary number must consist of only 0s and 1s")
+        if len(digits) != 8:
+            raise ValueError("Binary number must be 8 digits long")
+
+
+if __name__ == "__main__":
+    display = SevenSegmentDisplay()
+    print(display.display_segment("11111101"))
+    print(display.display_segment("01100001"))
+    print(display.display_segment("11011011"))
+    print(display.display_segment("11110011"))
+
---
+++ assignments/february2025/sevensegment/tests/testsevensegment.py
@@ -0,0 +1,142 @@
+import unittest
+
+from src.sevensegment import SevenSegmentDisplay
+
+
+class TestSevenSegmentDisplay(unittest.TestCase):
+    def setUp(self):
+        self.display = SevenSegmentDisplay()
+
+    def test_user_inputs_non_digit_numbers(self):
+        with self.assertRaises(ValueError):
+            self.display.display_segment("1234s678")
+
+    def test_user_inputs_more_than_8_digit_numbers(self):
+        with self.assertRaises(ValueError):
+            self.display.display_segment("010011101")
+
+    def test_user_inputs_less_than_8_digit_numbers(self):
+        with self.assertRaises(ValueError):
+            self.display.display_segment("1110001")
+
+    def test_given_11111100_board_is_off(self):
+        self.display.display_segment("11111100")
+        self.assertFalse(self.display.get_is_on())
+
+    def test_given_11111101_board_is_on(self):
+        self.display.display_segment("11111101")
+        self.assertTrue(self.display.get_is_on())
+        self.display.display_segment("11111100")
+        self.assertFalse(self.display.get_is_on())
+
+    def test_given_11111100_is_off_then_empty_string_is_displayed(self):
+        self.assertFalse(self.display.get_is_on())
+        expected_result = [[' ', ' ', ' ', ' '],
+                           [' ', ' ', ' ', ' '],
+                           [' ', ' ', ' ', ' '],
+                           [' ', ' ', ' ', ' '],
+                           [' ', ' ', ' ', ' ']]
+        self.assertEqual(expected_result, self.display.display_segment("11111100"))
+
+    def test_given_11111101_board_is_on_when_displayed_then_hashtags_representing_0_is_displayed(self):
+        actual = self.display.display_segment("11111101")
+        self.assertTrue(self.display.get_is_on())
+        expected = [['#', '#', '#', '#'],
+                    ['#', ' ', ' ', '#'],
+                    ['#', ' ', ' ', '#'],
+                    ['#', ' ', ' ', '#'],
+                    ['#', '#', '#', '#']]
+        self.assertEqual(expected, actual)
+
+    def test_given_01100001_board_is_on_when_displayed_then_hashtags_representing_1_is_displayed(self):
+        actual = self.display.display_segment("01100001")
+        self.assertTrue(self.display.get_is_on())
+        expected = [[' ', ' ', ' ', ' '],
+                    ['#', ' ', ' ', '#'],
+                    ['#', ' ', ' ', '#'],
+                    [' ', ' ', ' ', '#'],
+                    [' ', ' ', ' ', ' ']]
+        self.assertEqual(expected, actual)
+
+    def test_given_11011011_board_is_on_when_displayed_then_hashtags_representing_2_is_displayed(self):
+        actual = self.display.display_segment("11011011")
+        self.assertTrue(self.display.get_is_on())
+        expected = [['#', '#', '#', '#'],
+                    ['#', ' ', ' ', '#'],
+                    ['#', '#', '#', '#'],
+                    [' ', ' ', ' ', ' '],
+                    ['#', '#', '#', '#']]
+        self.assertEqual(expected, actual)
+
+    def test_given_11110011_board_is_on_when_displayed_then_hashtags_representing_3_is_displayed(self):
+        actual = self.display.display_segment("11110011")
+        self.assertTrue(self.display.get_is_on())
+        expected = [['#', '#', '#', '#'],
+                    ['#', ' ', ' ', '#'],
+                    ['#', '#', '#', '#'],
+                    [' ', ' ', ' ', '#'],
+                    ['#', '#', '#', '#']]
+        self.assertEqual(expected, actual)
+
+    def test_given_01100111_board_is_on_when_displayed_then_hashtags_representing_4_is_displayed(self):
+        actual = self.display.display_segment("01100111")
+        self.assertTrue(self.display.get_is_on())
+        expected = [[' ', ' ', ' ', ' '],
+                    ['#', ' ', ' ', '#'],
+                    ['#', '#', '#', '#'],
+                    [' ', ' ', ' ', '#'],
+                    [' ', ' ', ' ', ' ']]
+        self.assertEqual(expected, actual)
+
+    def test_given_10110111_board_is_on_when_displayed_then_hashtags_representing_5_is_displayed(self):
+        actual = self.display.display_segment("10110111")
+        self.assertTrue(self.display.get_is_on())
+        expected = [['#', '#', '#', '#'],
+                    [' ', ' ', ' ', ' '],
+                    ['#', '#', '#', '#'],
+                    ['#', ' ', ' ', '#'],
+                    ['#', '#', '#', '#']]
+        self.assertEqual(expected, actual)
+
+    def test_given_10111111_board_is_on_when_displayed_then_hashtags_representing_6_is_displayed(self):
+        actual = self.display.display_segment("10111111")
+        self.assertTrue(self.display.get_is_on())
+        expected = [['#', '#', '#', '#'],
+                    ['#', ' ', ' ', ' '],
+                    ['#', '#', '#', '#'],
+                    ['#', ' ', ' ', '#'],
+                    ['#', '#', '#', '#']]
+        self.assertEqual(expected, actual)
+
+    def test_given_11100001_board_is_on_when_displayed_then_hashtags_representing_7_is_displayed(self):
+        actual = self.display.display_segment("11100001")
+        self.assertTrue(self.display.get_is_on())
+        expected = [['#', '#', '#', '#'],
+                    ['#', ' ', ' ', '#'],
+                    ['#', ' ', ' ', '#'],
+                    [' ', ' ', ' ', '#'],
+                    [' ', ' ', ' ', ' ']]
+        self.assertEqual(expected, actual)
+
+    def test_given_11111111_board_is_on_when_displayed_then_hashtags_representing_8_is_displayed(self):
+        actual = self.display.display_segment("11111111")
+        self.assertTrue(self.display.get_is_on())
+        expected = [['#', '#', '#', '#'],
+                    ['#', ' ', ' ', '#'],
+                    ['#', '#', '#', '#'],
+                    ['#', ' ', ' ', '#'],
+                    ['#', '#', '#', '#']]
+        self.assertEqual(expected, actual)
+
+    def test_given_11110111_board_is_on_when_displayed_then_hashtags_representing_9_is_displayed(self):
+        actual = self.display.display_segment("11110111")
+        self.assertTrue(self.display.get_is_on())
+        expected = [['#', '#', '#', '#'],
+                    ['#', ' ', ' ', '#'],
+                    ['#', '#', '#', '#'],
+                    [' ', ' ', ' ', '#'],
+                    ['#', '#', '#', '#']]
+        self.assertEqual(expected, actual)
+
+if __name__ == '__main__':
+    unittest.main()
