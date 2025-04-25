9ab4f1559f5e6dcf7754e1ab2fea825af798bda3
Mercy Babayemi <mercyjanet013@gmail.com>
2025-04-25 14:31:41 +0100

    adding task

---
+++ classtask/april2025/binaryconversiontask/test/binary_conversion_test.py
@@ -0,0 +1,16 @@
+import unittest
+
+import binary_conversion
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_that_function_exist(self):
+        number = "10"
+        binary_conversion.convert_binary_to_base_ten(number)
+
+    def test_that_function_returns_correct_value(self):
+        self.assertEqual(2, binary_conversion.convert_binary_to_base_ten("10"))
+
+if __name__ == '__main__':
+    unittest.main()
