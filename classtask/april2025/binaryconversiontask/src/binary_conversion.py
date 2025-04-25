977aa47b5928933856ba737d0c688fcc7d329c82
Mercy Babayemi <mercyjanet013@gmail.com>
2025-04-26 00:05:37 +0100

    complete test

--- classtask/april2025/binaryconversiontask/src/binary_conversion.py
+++ classtask/april2025/binaryconversiontask/src/binary_conversion.py
@@ -4,16 +4,14 @@ from exceptions import value_not_binary_exception
 def convert_binary_to_base_ten(*binary: str):
     calculation: int = 0
     for value in binary:
+        length: int = len(value)
         for item in value:
-            length: int = len(value)
 
             if int(item) == 1 or int(item) == 0:
-                for each in item:
-                    calculated = int(each) * (2 ** (length - 1))
+                    calculated = int(item) * (2 ** (length - 1))
                     calculation += calculated
                     length -= 1
-                    if length == 0:
-                        return
+
             elif int(item) > 1:
                 raise value_not_binary_exception
 
--- classtask/april2025/binaryconversiontask/test/binary_conversion_test.py
+++ classtask/april2025/binaryconversiontask/test/binary_conversion_test.py
@@ -11,6 +11,7 @@ class MyTestCase(unittest.TestCase):
 
     def test_that_function_returns_correct_value(self):
         self.assertEqual(2, binary_conversion.convert_binary_to_base_ten("10"))
+        self.assertEqual(3, binary_conversion.convert_binary_to_base_ten("11"))
 
 if __name__ == '__main__':
     unittest.main()
